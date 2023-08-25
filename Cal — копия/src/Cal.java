62caa16718666461da17ea67068b0cdb0a72ebce
DuBronks <mikhalchenkov1@gmail.com>
2023-08-25 22:56:54 +0700

    education
    education

---
+++ Cal — копия/src/Cal.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите арифметическое выражение соблюдая пробелы (например, 2 + 3):");
+        String input = scanner.nextLine();
+
+        try {
+            String result = calc(input);
+            System.out.println("Результат: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+
+    static String calc(String input) {
+        String[] parts = input.split(" ");
+
+        if (parts.length != 3) {
+            throw new IllegalArgumentException("Некорректное выражение");
+        }
+
+        int a = Integer.parseInt(parts[0]);
+        int b = Integer.parseInt(parts[2]);
+        String operator = parts[1];
+
+        if (a < 1 || a > 10 || b < 1 || b > 10) {
+            throw new IllegalArgumentException("Числа должны быть от 1 до 10 включительно");
+        }
+
+        int result;
+        switch (operator) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                if (b == 0) {
+                    throw new IllegalArgumentException("Деление на ноль");
+                }
+                result = a / b;
+                break;
+            default:
+                throw new IllegalArgumentException("Неподдерживаемая операция");
+        }
+
+        return Integer.toString(result);
+    }
+}
